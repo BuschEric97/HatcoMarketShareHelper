32a5dd1cf8dbd5ac026c106cfb39242767e2fa9e
Eric Busch <buscheric97@gmail.com>
2020-01-24 20:41:23 -0600

    Tweaked button and header text

--- HatcoMarketShareHelper/Form1.Designer.cs
+++ HatcoMarketShareHelper/Form1.Designer.cs
@@ -151,9 +151,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(4, 4);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.Size = new System.Drawing.Size(126, 20);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Determiner";
+            this.label1.Text = "Closing Analyzer";
             // 
             // runDeterminer
             // 
@@ -161,7 +161,7 @@
             this.runDeterminer.Name = "runDeterminer";
             this.runDeterminer.Size = new System.Drawing.Size(103, 23);
             this.runDeterminer.TabIndex = 0;
-            this.runDeterminer.Text = "Run Determiner";
+            this.runDeterminer.Text = "Run Analyzer";
             this.runDeterminer.UseVisualStyleBackColor = true;
             this.runDeterminer.Click += new System.EventHandler(this.runDeterminer_Click);
             // 
@@ -212,9 +212,9 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(4, 3);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.Size = new System.Drawing.Size(170, 20);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Processor";
+            this.label2.Text = "Market Share Analyzer";
             // 
             // processorProgressDetailed
             // 
@@ -231,7 +231,7 @@
             this.runProcessor.Name = "runProcessor";
             this.runProcessor.Size = new System.Drawing.Size(103, 23);
             this.runProcessor.TabIndex = 5;
-            this.runProcessor.Text = "Run Processor";
+            this.runProcessor.Text = "Run Analyzer";
             this.runProcessor.UseVisualStyleBackColor = true;
             this.runProcessor.Click += new System.EventHandler(this.runProcessor_Click);
             // 
@@ -364,9 +364,9 @@
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.Location = new System.Drawing.Point(4, 4);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(151, 20);
+            this.label8.Size = new System.Drawing.Size(189, 20);
             this.label8.TabIndex = 9;
-            this.label8.Text = "Determiner Settings";
+            this.label8.Text = "Closing Analyzer Settings";
             // 
             // MLSInputFile_Determiner
             // 
@@ -461,9 +461,9 @@
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.Location = new System.Drawing.Point(4, 4);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(143, 20);
+            this.label9.Size = new System.Drawing.Size(233, 20);
             this.label9.TabIndex = 10;
-            this.label9.Text = "Processor Settings";
+            this.label9.Text = "Market Share Analyzer Settings";
             // 
             // MLSInputFile_Processor
             // 
